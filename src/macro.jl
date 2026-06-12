bbc8ab25bdb0ff78751972404d197166fdea732f
vcorcoro <137559813+vcorcoro@users.noreply.github.com>
2026-06-12 14:05:59 -0700

    allow subtype of context

--- src/macro.jl
+++ src/macro.jl
@@ -498,7 +498,7 @@ end
 source(s::S) where {S<:System} = source(S)
 source(::Type{S}) where {S<:System} = source(typefor(S))
 source(::Type{System}) = quote
-    context ~ ::Context(override)
+    context ~ ::Type{<:Context}(override)
     config(context) => context.config ~ ::Config
 end
 source(::Type) = :()
